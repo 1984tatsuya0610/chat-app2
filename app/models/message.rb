61715a8d4d817d4e9a57eada8bf128759462df3c
1984tatsuya0610 <cloudblack793@gmail.com>
2022-11-13 21:44:00 +0900

    各モデルのテスト

--- app/models/message.rb
+++ app/models/message.rb
@@ -2,7 +2,6 @@ class Message < ApplicationRecord
   belongs_to :room
   belongs_to :user
   has_one_attached :image #MessagesテーブルとActive Storageのテーブルで管理された画像ファイルのアソシエーションを記述
-
   validates :content, presence: true, unless: :was_atttached? #空の場合はDBに保存しないというバリデーションを設定
 
   def was_atttached?
