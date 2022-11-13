2f6897d3014ab880ba82db8680cdc918816a3259
1984tatsuya0610 <cloudblack793@gmail.com>
2022-11-13 13:52:17 +0900

    メッセージ送信機能の実装

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -1,4 +1,26 @@
 class MessagesController < ApplicationController
   def index
+    @message = Message.new #メッセージモデルのインスタンス情報を代入
+    @room = Room.find(params[:room_id]) #pasamsに含まれているチャットルームのレコード情報「room_id」を代入
+  end
+
+  def create
+    @room = Room.find(params[:room_id])
+    @message = @room.messages.new(message_params)
+    if @message.save
+      redirect_to room_messages_path(@room)
+    else
+      render :index
+    end
+  end
+
+  private
+
+  def message_params
+    params.require(:message).permit(:content).merge(user_id: current_user.id)
   end
 end
+
+#praivateメソッドとしてmessage_paramsを定義し、
+#メッセージの内容contentをmessageテーブルへ保存できる様にする。
+#パラメーターの中に、ログインしているユーザーのidと紐付いている、メッセージの内容contentを受け取れるように許可する。
